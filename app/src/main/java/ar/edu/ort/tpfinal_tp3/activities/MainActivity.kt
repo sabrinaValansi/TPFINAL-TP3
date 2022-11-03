e123a3c5eb111813317476c709226d3a666d0abe
exerazor1911 <exe_cien@hotmail.com>
2022-11-02 23:28:25 -0300

    test

--- app/src/main/java/ar/edu/ort/tpfinal_tp3/activities/MainActivity.kt
+++ app/src/main/java/ar/edu/ort/tpfinal_tp3/activities/MainActivity.kt
@@ -3,22 +3,50 @@ package ar.edu.ort.tpfinal_tp3.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import ar.edu.ort.tpfinal_tp3.R
 import ar.edu.ort.tpfinal_tp3.utils.UserSession
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+    private lateinit var appBarConfig : AppBarConfiguration
+    private lateinit var navView : NavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        initSession()
+        initSessionAndHamgMenu()
     }
 
 
-    private fun initSession() {
+    private fun initSessionAndHamgMenu() {
+
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+
+        navView = findViewById(R.id.nav_view)
+
+        navView.setupWithNavController(navController)
+
+
+        appBarConfig = AppBarConfiguration(
+            topLevelDestinationIds = setOf(
+                R.id.home2,
+                R.id.settingsActivity,
+                R.id.login
+            ),
+            fallbackOnNavigateUpListener = ::onSupportNavigateUp
+        )
+
+        setupActionBarWithNavController(navController, appBarConfig)
 
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
 
