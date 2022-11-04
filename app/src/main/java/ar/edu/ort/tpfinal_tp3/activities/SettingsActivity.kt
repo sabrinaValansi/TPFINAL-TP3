1a73ed7e1e1e8630057bc7dec383c072e5f37aa6
exerazor1911 <exe_cien@hotmail.com>
2022-11-03 22:34:50 -0300

    agregada barrita back

--- app/src/main/java/ar/edu/ort/tpfinal_tp3/activities/SettingsActivity.kt
+++ app/src/main/java/ar/edu/ort/tpfinal_tp3/activities/SettingsActivity.kt
@@ -2,11 +2,38 @@ package ar.edu.ort.tpfinal_tp3.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.preference.PreferenceFragmentCompat
 import ar.edu.ort.tpfinal_tp3.R
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.settings, SettingsFragment())
+            .commit()
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true);
+    }
+
+    class SettingsFragment : PreferenceFragmentCompat() {
+        override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+            setPreferencesFromResource(R.xml.user_settings, rootKey)
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle presses on the action bar menu items
+        when (item.itemId) {
+            android.R.id.home -> {
+                onBackPressed()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
